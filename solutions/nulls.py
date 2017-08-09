1c59cfb4e6a244cb19ade0e8432414b306237228
kjam <kjarmul@gmail.com>
2017-08-09 13:54:00 +0200

    updates for data and solutions for next course (adding future dates, more lobsters stories and removing solutions from cells)

--- solutions/nulls.py
+++ solutions/nulls.py
@@ -1,2 +1,2 @@
-rows_filled = 32357 - df[df.temperature.isnull() == True].shape[0] # taken from cell 19
+rows_filled = 32357 - df[df.temperature.isnull() == True].shape[0] # taken from cell 15
 still_missing = df[df.temperature.isnull() == True].shape[0] / df.shape[0]
