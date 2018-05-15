e7acc23fd4552416e04fe1fc5377acac4f712ee6
kjam <kjarmul@gmail.com>
2018-05-15 15:01:01 +0200

    adding cleaned up notebooks

--- solutions/engarde.py
+++ solutions/engarde.py
@@ -3,7 +3,7 @@
 def calculate_store_sales(sales):
     sales['store_total'] = sales.groupby('store_id').transform(sum)['sale_amount']
     sales['associate_total'] = sales.groupby('associate').transform(sum)['sale_amount']
-    sales['city_total'] = sales.groupby('city').transform(sum)['sale_amount']
+    sales['city_total'] = sales.groupby('city')['sale_amount'].transform(sum)
     sales['store_total'] = pd.to_numeric(sales['store_total'])
     sales['city_total'] = pd.to_numeric(sales['city_total'])
     sales['associate_total'] = pd.to_numeric(sales['associate_total'])
